0067fc39a371e7f084b8940bc5f7cafac84a1b69
johnodhiambo0 <johneliud4@gmail.com>
2024-11-21 14:26:39 +0300

    add: function removeClientFromChat that will remove a client from the chat upon termination.

---
+++ utils/remove-client-from-chat.go
@@ -0,0 +1,17 @@
+package utils
+
+import "net"
+
+// Will remove a client from the chat upon termination by the client.
+func removeClientFromChat(conn net.Conn) {
+	mu.Lock()
+	defer mu.Unlock()
+
+	for i, client := range clientsList {
+		if client.conn == conn {
+			client.conn.Close()
+			clientsList = append(clientsList[:i], clientsList[i+1:]...)
+			break
+		}
+	}
+}
